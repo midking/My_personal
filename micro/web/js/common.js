612add754b451f4af8b7822ba1ce5ea1ca6a0cbb
midking <cizel444@gmail.com>
2016-10-13 08:23:41 +0900

    마이크로 메인 폰트 스크립트 변경

--- micro/web/js/common.js
+++ micro/web/js/common.js
@@ -18,37 +18,44 @@ $(document).ready(function(){
 		}else{
 			$('.topBox').removeClass('scrollTop');
 		}
+		console.log(scrollTop);
 	});
+
+	var explode = function(){
+		console.log("asdf");
+		$("body").animate({scrollTop:373}, 2500);
+	}
+	setTimeout(explode, 2000);
 
 
 	/* 메인 백그라운드 마우스 오버시 move */
-	// var lFollowX = 0,
-	// 	lFollowY = 0,
-	// 	x = 0,
-	// 	y = 0,
-	// 	friction = 1 / 30;
+	/*var lFollowX = 0,
+		lFollowY = 0,
+		x = 0,
+		y = 0,
+		friction = 1 / 30;
 
-	// function moveBackground() {
-	// 	x += (lFollowX - x) * friction;
-	// 	y += (lFollowY - y) * friction;
+	function moveBackground() {
+		x += (lFollowX - x) * friction;
+		y += (lFollowY - y) * friction;
 
-	// 	translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.05)';
+		translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.05)';
 		
-	// 	$('.mainBg').css({
-	// 		'-webit-transform': translate,
-	// 		'-moz-transform': translate,
-	// 		'transform': translate
-	// 	});
+		$('.mainBg').css({
+			'-webit-transform': translate,
+			'-moz-transform': translate,
+			'transform': translate
+		});
 
-	// 	window.requestAnimationFrame(moveBackground);
-	// }
+		window.requestAnimationFrame(moveBackground);
+	}
 
-	// $(window).on('mousemove click', function(e) {
-	// 	var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-	// 	var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-	// 	lFollowX = (6 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-	// 	lFollowY = (2 * lMouseY) / 100;
-	// });
-	// moveBackground();
+	$(window).on('mousemove click', function(e) {
+		var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+		var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+		lFollowX = (6 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+		lFollowY = (2 * lMouseY) / 100;
+	});
+	moveBackground();*/
 	/* 메인 백그라운드 마우스 오버시 move */
 });
